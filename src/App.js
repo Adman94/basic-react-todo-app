a68aa9786097d2eaf684810026ae656946fde39a
Adeeb Rahman <adman00794@gmail.com>
2025-09-02 02:29:17 -0500

    newly remodeled todo list with crud (#1)
    Co-authored-by: coderzden <coderzden23@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,72 +1,11 @@
+// import logo from "./logo.svg";
 import "./App.css";
-import React, { useState, useRef, useEffect } from "react";
-import TodoList from "./TodoList";
-import { v4 as uuidv4 } from "uuid";
-import Button from "@mui/material/Button";
-
-const LOCAL_STORAGE_KEY = "todoApp.todos";
+import TodoList from "./features/todos/TodoList";
 
 function App() {
-  const [todos, setTodos] = useState([]);
-  const todoNameRef = useRef();
-
-  useEffect(() => {
-    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-    if (storedTodos) setTodos(storedTodos);
-  }, []);
-
-  useEffect(() => {
-    if (todos.length !== 0)
-      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
-  }, [todos]);
-
-  function toggleTodo(id) {
-    const newTodos = [...todos];
-    const todo = newTodos.find((todo) => todo.id === id);
-    todo.complete = !todo.complete;
-    setTodos(newTodos);
-  }
-
-  function handleAddTodo(e) {
-    const name = todoNameRef.current.value;
-    if (name === "") return;
-    setTodos((prevTodos) => {
-      return [...prevTodos, { id: uuidv4(), name: name, complete: false }];
-    });
-    todoNameRef.current.value = null;
-  }
-
-  function handleClearTodos() {
-    const newTodos = todos.filter((todo) => !todo.complete);
-    setTodos(newTodos);
-  }
-
   return (
     <div className="App">
-      <h1>DAILY TODO</h1>
-      <div className="container">
-        <TodoList todos={todos} toggleTodo={toggleTodo} />
-        
-        <div className="controls">
-        <div className="input">
-          <input
-            ref={todoNameRef}
-            id="outlined-basic"
-            placeholder="order food, take out trash, etc."
-            variant="outlined"
-          />
-        </div>
-          <Button onClick={handleAddTodo} variant="outlined">
-            Add Todo
-          </Button>
-          <Button onClick={handleClearTodos} variant="outlined">
-            Clear Completed Todos
-          </Button>
-        </div>
-        <div className="todo-left">
-        {todos.filter((todo) => !todo.complete).length} todo left
-      </div>
-      </div>
+      <TodoList />
     </div>
   );
 }
